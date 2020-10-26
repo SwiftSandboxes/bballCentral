4bbae8776fbba38feca4e5a6323d811fea51f99a
Notso <generalemail7@yahoo.com>
2020-10-25 21:48:50 -0500

    fix git completion curl

--- cicdServerSetup.sh
+++ cicdServerSetup.sh
@@ -8,5 +8,5 @@
 # curl -O https://gist.githubusercontent.com/Notso/feacb7a6c5e7d89a2bc75b5ed25c9adc/raw/612645fa7adb9c5bea1e04a2bd1bdbce5e7ef302/.bash_prompt
 # cp .bashrc .bashrc_orig
 # curl https://gist.githubusercontent.com/Notso/72407b8c7a56fd2043d3dad61f95055c/raw/1c1d6aed8a570dd5eabebb8cc645fedb72f637b3/.bashrc >> .bashrc
-# curl -O https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+# curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >> .git_completion
 # curl https://gist.githubusercontent.com/Notso/92c6fe5d1504b025782a46a3fdf8971c/raw/926730ccc06965c0245cb8028e58270a14d08de4/.bash_profile >> .bash_profile
