647269a15e24aa3669ca2f63bac20b813ebe9d08
Notso <generalemail7@yahoo.com>
2020-10-25 20:54:51 -0500

    Create cicdServerSetup.sh
    intended to be a script that could be run to set up a bare Linux Mint machine for running CICD tasks

---
+++ cicdServerSetup.sh
@@ -0,0 +1,5 @@
+# this script is intended to keep track of the steps done to setup the CICD server. 
+# Many of the steps may be commented out to begin with just to serve as documentation. However, the intention is that this could be runnable at some point.
+
+# Add VIM
+# apt update
