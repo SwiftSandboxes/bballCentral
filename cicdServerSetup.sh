6f6d55ba2000e278a66a8adb185d2eff39d0f844
Notso <generalemail7@yahoo.com>
2020-10-26 22:28:49 -0500

    Add docker

--- cicdServerSetup.sh
+++ cicdServerSetup.sh
@@ -12,6 +12,23 @@ curl https://gist.githubusercontent.com/Notso/72407b8c7a56fd2043d3dad61f95055c/r
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash >> .git_completion
 curl https://gist.githubusercontent.com/Notso/92c6fe5d1504b025782a46a3fdf8971c/raw/b98f75273079387c31e0a8a9c43cd9879a3424fa/.bash_profile >> .bash_profile
 
+# add Docker
+# note that this follows the steps on https://linuxhint.com/install_docker_linux_mint/ 
+# which is the same as https://docs.docker.com/engine/install/ubuntu/ except that the particular Ubuntu version (in this case focal) is explicitly 
+# defined since the $(lsb_release -cs) command would return the linux mint release version and not the proper Ubuntu release version
+# note that this does mean that when the Ubuntu version updates, this script may need to be updated
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 # add minikube
 curl -o Downloads/minikube_latest_amd64.deb -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 # note that the below command may prompt for root user password
